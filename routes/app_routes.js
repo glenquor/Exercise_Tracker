13e4f4b7df27587ba80356c6da08ecf87d4ebcbe
Glen Quirós Ortiz <glenquor@gmail.com>
2022-01-26 22:42:54 -0600

    all routes are working and saving the data perfectly, there is only one route left to do

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const Excercise = require('../models/excercises');
+const { Exercise, User } = require('../models/exercises');
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
@@ -10,66 +10,74 @@ router.get('/', (req, res) => {
 
 router.post('/api/users', async (req,res) =>{
   const user = req.body.username;
-  const userExist = await Excercise.exists({ username: user });
+  const userExist = await User.exists({ username: user });
   if (userExist) {
     res.json({ status: "This user already exist" });
   } else {
-      const newUser = new Excercise ({
-    username: req.body.username
-  });
-  await newUser.save();
-  res.json(newUser);
+    const newUser = new User ({
+      username: req.body.username,
+    });
+    await newUser.save((err,data) => {
+      if (err) {
+        res.send('There was an error: ', err);
+      }
+      res.json({
+        "_id": data._id,
+        "username": data.username
+      });
+    });
   }
 });
 
 router.get('/api/users', async (req,res) =>{
-  const allUser = await Excercise.find();
-  res.json(allUser);
+  const allUsers = await User.find();
+  res.json(allUsers);
 });
 
 router.post('/api/users/:_id/exercises', async (req,res) =>{
-  if (req.body.date === '' || (typeof req.body.date === 'undefined')) {
-    await Excercise.findByIdAndUpdate({ _id: req.params._id },{
+  if (req.body.date === '' || (typeof req.body.date === 'undefined' || req.body.date === 'Invalid Date')) {
+    const newExercise = new Exercise ({
       description: req.body.description, 
       duration: req.body.duration, 
       date: new Date().toDateString()
+    });  
+    await newExercise.save();
+    const userFound = await User.findById(req.params._id);
+    userFound.count = userFound.count + 1;
+    userFound.log.push(newExercise);
+    const userUpdated = await userFound.save();
+    res.json({
+      "_id": userUpdated._id,
+      "username": userUpdated.username,
+      "date": new Date(newExercise.date).toDateString(),
+      "duration": newExercise.duration,
+      "description":newExercise.description
     });
-    await Excercise.findOne({ _id: req.params._id }, (err,userFound) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(new Date(userFound.date).toDateString());
-      res.json({
-        "_id": userFound._id,
-        "username": userFound.username,
-        "date": new Date(userFound.date).toDateString(),
-        "duration": userFound.duration,
-        "description": userFound.description
-      });
-    }).clone().catch(function(err){ console.log(err)});
-  } else {
-    await Excercise.findByIdAndUpdate({ _id: req.params._id },{
+  }
+  else {
+    const newExercise = new Exercise ({
       description: req.body.description, 
       duration: req.body.duration, 
       date: new Date(req.body.date).toDateString()
+    });  
+    await newExercise.save();
+    const userFound = await User.findById(req.params._id);
+    userFound.count = userFound.count + 1;
+    userFound.log.push(newExercise);
+    const userUpdated = await userFound.save();
+    res.json({
+      "_id": userUpdated._id,
+      "username": userUpdated.username,
+      "date": new Date(newExercise.date).toDateString(),
+      "duration": newExercise.duration,
+      "description":newExercise.description
     });
-    await Excercise.findOne({ _id: req.params._id }, (err,userFound) => {
-      if (err) {
-        return console.log(err);
-      }
-      res.json({
-        "_id": userFound._id,
-        "username": userFound.username,
-        "date": new Date(userFound.date).toDateString(),
-        "duration": userFound.duration,
-        "description": userFound.description
-      });
-    }).clone().catch(function(err){ console.log(err)});
   }
 });
 
-// router.get('/api/users/:_id/logs', (req,res) => {
-
-// });
+// this route is not finished
+router.get('/api/users/:_id/logs', (req,res) => {
+  res.send('Hello');
+});
 
 module.exports = router;
