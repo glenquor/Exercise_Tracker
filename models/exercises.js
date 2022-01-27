96d7ba11593f80e96d8236c2cc5951806ce21c25
Glen Quirós Ortiz <glenquor@gmail.com>
2022-01-26 22:40:04 -0600

    now there are two models

--- models/exercises.js
+++ models/exercises.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../sample.env') });
 
 mongoose.connect(process.env.MONGO_URI);
 
-const exercisesSchema = new Schema ({
-  username: { type: String, required: true },
+let exercisesSchema = new Schema ({
   description: String,
   duration: Number,
   date: Date
 });
 
-module.exports = mongoose.model('exercises', exercisesSchema);
+let userSchema = new Schema ({
+  username: { type: String, required: true },
+  count: {type: Number, default: 0},
+  log: [exercisesSchema]
+});
+
+let Exercise = mongoose.model('exercises', exercisesSchema);
+let User = mongoose.model('users', userSchema);
+
+module.exports = { Exercise, User };
